0f23c051e291a96ff9066582c94b36caa554c4c4
Guil. Sperb Machado <guil@axlabs.com>
2022-11-18 18:25:14 +0100

    example about how different version of neow3j can co-exist in the same project

--- src/main/java/com/axlabs/boilerplate/SubscribeToBlocks.java
+++ src/main/java/com/axlabs/boilerplate/SubscribeToBlocks.java
@@ -9,10 +9,10 @@ public class SubscribeToBlocks {
 
     public static void main(String[] args) throws IOException {
 
-        // Initialize Neow3j to connect to a testnet Neo node.
+        // Initialize Neow3j to connect to a Neo N3 node.
         Neow3j neow3j = Neow3j.build(new HttpService("http://seed2t5.neo.org:20332"));
 
-        // Subscribe to new blocks on the testnet.
+        // Subscribe to new blocks on the Neo N3 network.
         neow3j.subscribeToNewBlocksObservable(true)
                 .subscribe((blockReqResult) -> {
                     System.out.println("#######################################");
@@ -22,6 +22,22 @@ public class SubscribeToBlocks {
                     System.out.println("Next Consensus:  " + blockReqResult.getBlock().getNextConsensus());
                     System.out.println("Transactions:    " + blockReqResult.getBlock().getTransactions());
                 });
+
+
+        // Initialize Neow3j to connect to a Neo Legacy node.
+        io.neow3j.legacy.protocol.Neow3j neow3jLegacy =
+                io.neow3j.legacy.protocol.Neow3j.build(
+                        new io.neow3j.legacy.protocol.http.HttpService("http://seed1.ngd.network:10332"));
+
+        // Subscribe to new blocks on the Neo Legacy network.
+        neow3jLegacy.catchUpToLatestAndSubscribeToNewBlocksObservable(io.neow3j.legacy.protocol.core.BlockParameterName.LATEST, true)
+            .subscribe((blockReqResult) -> {
+                System.out.println("#######################################");
+                System.out.println("blockIndex: " + blockReqResult.getBlock().getIndex());
+                System.out.println("hashId: " + blockReqResult.getBlock().getHash());
+                System.out.println("confirmations: " + blockReqResult.getBlock().getConfirmations());
+                System.out.println("transactions: " + blockReqResult.getBlock().getTransactions());
+            });
     }
 
 }
